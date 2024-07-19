218079456c57062aff6c3b3b50ac08f5dc219c3b
Edwared Yang <herenvarno@live.com>
2024-07-19 23:26:29 +0200

    remove useless files

--- module/vs-component/target/debug/deps/log-9e1b077fb5592a04.d
+++
@@ -1,8 +0,0 @@
-/home/herenvarno/Projects/github.com/vesyla-suite-4/module/vs-component/target/debug/deps/liblog-9e1b077fb5592a04.rmeta: /home/herenvarno/.cargo/registry/src/index.crates.io-6f17d22bba15001f/log-0.4.21/src/lib.rs /home/herenvarno/.cargo/registry/src/index.crates.io-6f17d22bba15001f/log-0.4.21/src/macros.rs /home/herenvarno/.cargo/registry/src/index.crates.io-6f17d22bba15001f/log-0.4.21/src/serde.rs /home/herenvarno/.cargo/registry/src/index.crates.io-6f17d22bba15001f/log-0.4.21/src/__private_api.rs
-
-/home/herenvarno/Projects/github.com/vesyla-suite-4/module/vs-component/target/debug/deps/log-9e1b077fb5592a04.d: /home/herenvarno/.cargo/registry/src/index.crates.io-6f17d22bba15001f/log-0.4.21/src/lib.rs /home/herenvarno/.cargo/registry/src/index.crates.io-6f17d22bba15001f/log-0.4.21/src/macros.rs /home/herenvarno/.cargo/registry/src/index.crates.io-6f17d22bba15001f/log-0.4.21/src/serde.rs /home/herenvarno/.cargo/registry/src/index.crates.io-6f17d22bba15001f/log-0.4.21/src/__private_api.rs
-
-/home/herenvarno/.cargo/registry/src/index.crates.io-6f17d22bba15001f/log-0.4.21/src/lib.rs:
-/home/herenvarno/.cargo/registry/src/index.crates.io-6f17d22bba15001f/log-0.4.21/src/macros.rs:
-/home/herenvarno/.cargo/registry/src/index.crates.io-6f17d22bba15001f/log-0.4.21/src/serde.rs:
-/home/herenvarno/.cargo/registry/src/index.crates.io-6f17d22bba15001f/log-0.4.21/src/__private_api.rs:
--- module/vs-component/target/debug/deps/vs_component-9f70ffafe7bd4e41.d
+++
@@ -1,7 +0,0 @@
-/home/herenvarno/Projects/github.com/vesyla-suite-4/module/vs-component/target/debug/deps/libvs_component-9f70ffafe7bd4e41.rmeta: src/main.rs src/collector.rs src/generator.rs
-
-/home/herenvarno/Projects/github.com/vesyla-suite-4/module/vs-component/target/debug/deps/vs_component-9f70ffafe7bd4e41.d: src/main.rs src/collector.rs src/generator.rs
-
-src/main.rs:
-src/collector.rs:
-src/generator.rs:
