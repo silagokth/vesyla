19eeff79e678122a75926b545f6a92a4d4cc95e3
Edwared Yang <herenvarno@live.com>
2025-02-04 17:00:23 +0100

    v4.0.0

--- make_appimage.sh
+++ make_appimage.sh
@@ -4,17 +4,21 @@
 
 set -e
 
-# compile the application to a fakeroot directory: appdir
-mkdir -p build
-cd build
+BUILDDIR=$(pwd)/build
+APPDIR=$BUILDDIR/appdir
+mkdir -p ${BUILDDIR}
+mkdir -p ${APPDIR}
+
+# compile the application to a fakeroot directory: $APPDIR
+cd $BUILDDIR
 cmake -DCMAKE_INSTALL_PREFIX=/usr ..
 make -j$(nproc)
-make DESTDIR=appdir install
+make DESTDIR=$APPDIR install
 
 # download the linuxdeploy tool
 wget -c https://github.com/linuxdeploy/linuxdeploy/releases/download/continuous/linuxdeploy-x86_64.AppImage
 chmod +x linuxdeploy-x86_64.AppImage
 
 # create the AppImage and rename it to vesyla-suite
-./linuxdeploy-x86_64.AppImage --appdir appdir --output appimage
+./linuxdeploy-x86_64.AppImage --appdir $APPDIR --output appimage
 mv vesyla-suite-x86_64.AppImage ../vesyla-suite
