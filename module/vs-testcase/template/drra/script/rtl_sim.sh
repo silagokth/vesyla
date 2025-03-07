68f10242054bd1ef52fa80591a25992ee227379c
Edwared Yang <herenvarno@live.com>
2025-03-07 14:22:05 +0100

    change script in template

--- module/vs-testcase/template/drra/script/rtl_sim.sh
+++ module/vs-testcase/template/drra/script/rtl_sim.sh
@@ -45,7 +45,7 @@ echo "exit" >> read_src.do
 vsim -c -do read_src.do
 vsim -c -voptargs=+acc -debugDB -do "log * -r;run -all" work.fabric_tb
 cd ..
-cp temp/sram_image_out.bin mem/sram_image_m2.bin
+cp temp/sram_image_out.bin mem/sram_image_m3.bin
 
 # archive everything
 mv temp archive/rtl_sim_${id}
