b64d0ce19fead9d1871a91fcb4c402c79d88bf80
Paul Delestrac <paul.delestrac@gmail.com>
2025-04-29 20:10:04 +0200

    added protoc to the appimage install; changed the protoc path in vs-schedule cmake; updated protofbuf and ortools python versions

--- scripts/make_appimage.sh
+++ scripts/make_appimage.sh
@@ -6,12 +6,15 @@ set -e
 # set -x
 
 # Get paths
-SCRIPTDIR="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P)"
+SCRIPTDIR="$(
+  cd -- "$(dirname "$0")" >/dev/null 2>&1
+  pwd -P
+)"
 BUILDDIR=$SCRIPTDIR/../build
 APPDIR=$BUILDDIR/appdir
 LOGFILE=${BUILDDIR}/build.log
 if [ -f $LOGFILE ]; then
-    rm -f $LOGFILE
+  rm -f $LOGFILE
 fi
 
 # Create directories
@@ -26,39 +29,39 @@ touch $LOGFILE
 # Direct all stdout and stderr to the log file
 exec > >(tee -a $LOGFILE) 2>&1
 
-# Function to print log messages. 
+# Function to print log messages.
 # It takes two arguments: the log level and the message.
 # It prints the message to the console and appends it to the log file.
 log() {
-    RED='\033[0;31m'
-    GREEN='\033[0;32m'
-    YELLOW='\033[0;33m'
-    BLUE='\033[0;34m'
-    NC='\033[0m'
-    # check if it has two arguments
-    if [ "$#" -ne 2 ]; then
-        echo "${RED}[fatal]${NC}: Invalid number of arguments. Usage: log <level> <message>"
-        exit 1
-    fi
-    local level=$1
-    local message=$2
-    if [ "$level" = "info" ]; then
-        echo -e "${GREEN}[$level]${NC}: $message"
-    elif [ "$level" = "warn" ]; then
-        echo -e "${YELLOW}[$level]${NC}: $message"
-    elif [ "$level" = "error" ]; then
-        echo -e "${RED}[$level]${NC}: $message"
-    elif [ "$level" = "fatal" ]; then
-        echo -e "${RED}[$level]${NC}: $message"
-    else
-        echo "${RED}[fatal]${NC}: Invalid log level: $level"
-        exit 1
-    fi
-    if [ "$level" = "fatal" ]; then
-        echo "${RED}[fatal]${NC}: Fatal error detected, exiting..."
-        echo "${RED}[fatal]${NC}: Check the ${LOGFILE} to find out the detailed error!"
-        exit 1
-    fi
+  RED='\033[0;31m'
+  GREEN='\033[0;32m'
+  YELLOW='\033[0;33m'
+  BLUE='\033[0;34m'
+  NC='\033[0m'
+  # check if it has two arguments
+  if [ "$#" -ne 2 ]; then
+    echo "${RED}[fatal]${NC}: Invalid number of arguments. Usage: log <level> <message>"
+    exit 1
+  fi
+  local level=$1
+  local message=$2
+  if [ "$level" = "info" ]; then
+    echo -e "${GREEN}[$level]${NC}: $message"
+  elif [ "$level" = "warn" ]; then
+    echo -e "${YELLOW}[$level]${NC}: $message"
+  elif [ "$level" = "error" ]; then
+    echo -e "${RED}[$level]${NC}: $message"
+  elif [ "$level" = "fatal" ]; then
+    echo -e "${RED}[$level]${NC}: $message"
+  else
+    echo "${RED}[fatal]${NC}: Invalid log level: $level"
+    exit 1
+  fi
+  if [ "$level" = "fatal" ]; then
+    echo "${RED}[fatal]${NC}: Fatal error detected, exiting..."
+    echo "${RED}[fatal]${NC}: Check the ${LOGFILE} to find out the detailed error!"
+    exit 1
+  fi
 }
 
 cd $BUILDDIR
@@ -73,6 +76,21 @@ else
   log info "Rust is already installed. Skipping installation."
 fi
 
+# Install protobuf-compiler from release
+if [ ! -f $APPDIR/usr/bin/protoc ]; then
+  PROTOC_FILE=protoc-29.3-linux-x86_64.zip
+  PROTOC_VERSION=v29.3
+  PB_REL="https://github.com/protocolbuffers/protobuf/releases"
+  curl -LO $PB_REL/download/$PROTOC_VERSION/$PROTOC_FILE
+  unzip -o $PROTOC_FILE -d ./protoc/
+  rm -f $PROTOC_FILE
+  if [ ! -d $APPDIR/usr/bin ]; then
+    mkdir -p $APPDIR/usr/bin
+  fi
+  cp -f protoc/bin/protoc $APPDIR/usr/bin/
+  chmod +x $APPDIR/usr/bin/protoc
+fi
+
 # Install python library and requirements
 log info "Installing python library and requirements..."
 python3 -m venv venv
@@ -82,8 +100,8 @@ pip3 install -r $SCRIPTDIR/requirements.txt
 
 # Compile the application to a fakeroot directory: $APPDIR
 log info "Compiling the application..."
-cmake -DCMAKE_INSTALL_PREFIX=/usr ..
-make -j$(nproc)
+cmake -DCMAKE_INSTALL_PREFIX=/usr -DPROTOC_PATH=$APPDIR/usr/bin/protoc ..
+cmake --build .
 make DESTDIR=$APPDIR install
 
 # Download the linuxdeploy tool
