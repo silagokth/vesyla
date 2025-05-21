c27823fa8c17fa3312d3e9c828cd0328bc4f4f6b
Paul Delestrac <paul.delestrac@gmail.com>
2025-05-21 17:09:01 +0200

    chore(IO.hpp): add missing include guards for ARCH_IO_DEPTH and ARCH_IO_WIDTH

--- modules/vs-testcase/template/drra/model_0/include/IO.hpp
+++ modules/vs-testcase/template/drra/model_0/include/IO.hpp
@@ -1,6 +1,10 @@
 #include "Array.hpp"
 
+#ifndef ARCH_IO_DEPTH
 #define ARCH_IO_DEPTH 65536
+#endif
+#ifndef ARCH_IO_WIDTH
 #define ARCH_IO_WIDTH 256
+#endif
 
 typedef Array<ARCH_IO_DEPTH, ARCH_IO_WIDTH> IO;
