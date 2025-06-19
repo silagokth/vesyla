1f516f2f9f832731117ce95a5d4a287788f999a3
Edwared Yang <herenvarno@live.com>
2025-06-19 10:05:10 +0200

    add dependency flex and bison

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -8,18 +8,18 @@ set -x
 if [ -f /etc/lsb-release ]; then
   # Ubuntu
   sudo apt-get update
-  sudo apt-get install -y cmake curl python3 python3-pip python3-venv protobuf-compiler libprotobuf-dev wget libfuse2 file
+  sudo apt-get install -y cmake curl python3 python3-pip python3-venv protobuf-compiler libprotobuf-dev wget libfuse2 file flex bison
 elif [ -f /etc/debian_version ]; then
   # Debian
   sudo apt-get update
-  sudo apt-get install -y make gcc g++ cmake libboost-all-dev protobuf-compiler libprotobuf-dev python3 python3-pip libfuse2
+  sudo apt-get install -y make gcc g++ cmake libboost-all-dev protobuf-compiler libprotobuf-dev python3 python3-pip libfuse2 flex bison
 elif [ -f /etc/fedora-release ]; then
   # Fedora
-  sudo dnf install -y python python3-pip make gcc gcc-c++ cmake boost boost-devel protobuf protobuf-devel
+  sudo dnf install -y python python3-pip make gcc gcc-c++ cmake boost boost-devel protobuf protobuf-devel flex bison
 elif [ -f /etc/arch-release ]; then
   # Arch
-  sudo pacman -Syu --noconfirm make gcc cmake boost protobuf python python-pip
+  sudo pacman -Syu --noconfirm make gcc cmake boost protobuf python python-pip flex bison
 else
-  echo "Unsupported distribution! You need to install the dependencies manually. Check the requirements.txt."
+  echo "Unsupported distribution! You need to install the dependencies manually.
   exit 1
 fi
