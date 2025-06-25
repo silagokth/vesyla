370c793e0375d336dcb5f04d8f1ce21fa5bd6405
Paul Delestrac <paul.delestrac@gmail.com>
2025-06-25 16:12:46 +0200

    feat(vs-compile): Add CIR dialect to MLIR LSP server registry

--- modules/vs-compile/src/exec/pasm-mlir-lsp-server/main.cpp
+++ modules/vs-compile/src/exec/pasm-mlir-lsp-server/main.cpp
@@ -1,10 +1,19 @@
 #include "mlir/IR/DialectRegistry.h"
 #include "mlir/Tools/mlir-lsp-server/MlirLspServerMain.h"
 
+#include "clang/CIR/Dialect/IR/CIRDialect.h"
+
 #include "pasm/Dialect.hpp"
 
 int main(int argc, char **argv) {
   mlir::DialectRegistry registry;
+
+  // Add the CIR dialect to the registry
+  registry.insert<cir::CIRDialect>();
+
+  // Add the Pasm dialect to the registry
   registry.insert<vesyla::pasm::PasmDialect>();
+
+  // Register the dialects with the MLIR LSP server
   return mlir::failed(mlir::MlirLspServerMain(argc, argv, registry));
 }
